25e6140b1dacc67bb2f991bbac30c711a66434c8
Joe Clark <weirdlookingjay@live.com>
2019-08-01 16:14:52 -0400

    Created Home, Toolbar and Footer Components

---
+++ README.md
@@ -0,0 +1,1 @@
+# This is a real time single page forum app.
---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-toolbar dark color="primary">
+        <v-toolbar-title>Sandbox</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn flat>Forum</v-btn>
+            <v-btn flat>Ask Questions</v-btn>
+            <v-btn flat>Category</v-btn>
+            <v-btn flat>Login</v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
